5a05977fe600fb557325c0a734ef02632a583cda
ws783 <saraws783@gmail.com>
2025-05-05 12:55:42 +0300

    fix the server

--- Server/app/app.py
+++ Server/app/app.py
@@ -6,11 +6,6 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to the FastAPI server!"}
 
-
-@app.get("/{name}")
-def read(name: str):
-    return {"message": f"{name}!"}
-
 @app.get("/hello/{name}")
 def read_item(name: str):
     return {"message": f"Hello, {name}!"}
