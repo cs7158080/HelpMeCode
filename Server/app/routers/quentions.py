189bf28d2f48321f98b57ac1efd2b7e442424e17
chaya <..>
2025-05-14 10:09:13 +0300

    add lru-cache instead of create many object

--- Server/app/routers/quentions.py
+++ Server/app/routers/quentions.py
@@ -1,34 +1,19 @@
 from fastapi import APIRouter, Depends
 from db.Modules.quentions import Quentions
 from db.Services.dependencies import get_mongo_operations, get_db
+from functools import lru_cache
 
 router = APIRouter(tags=["quentions"])
 
-def create_quentions_service(
-    mongo_operations = Depends(lambda db=Depends(get_db): get_mongo_operations("quentions", db))
-):
-    """
-    Factory function to create a singleton Quentions service.
-    """
-    if not hasattr(create_quentions_service, "_instance"):
-        create_quentions_service._instance = Quentions(mongo_operations)
-    return create_quentions_service._instance
 
+@lru_cache()
 def get_quentions_service(
-    quentions_service=Depends(create_quentions_service)
+    mongo_operations=Depends(lambda db=Depends(get_db): get_mongo_operations("quentions", db))
 ):
     """
-    Dependency to provide a Quentions service.
+    Dependency to provide a Quentions service with caching.
     """
-    return quentions_service
-
-# def get_quentions_service(
-#     mongo_operations = Depends(lambda db=Depends(get_db): get_mongo_operations("quentions", db))
-# ):
-#     """
-#     Dependency to provide a Quentions service.
-#     """
-#     return Quentions(mongo_operations)
+    return Quentions(mongo_operations)
 
 @router.post("/createQuention")
 def create_quention(quention_data: dict, quentions_service=Depends(get_quentions_service)):
