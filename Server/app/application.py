b48a581e5747fec4a7cf2b439a4fa8ed21975f8c
ws783 <saraws783@gmail.com>
2025-05-14 20:34:32 +0300

    init branch

--- Server/app/application.py
+++ Server/app/application.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from routers.tags import router as tags_router 
-from routers.question import router as question_router 
+from routers.quentions import router as question_router
+from routers.users import router as users_router
 from db.Services.dependencies import startup_db_client, shutdown_db_client
-
 app1 = FastAPI()
 
 
 app1.include_router(tags_router, prefix="/tags")
 app1.include_router(users_router, prefix="/users")
+app1.include_router(question_router, prefix="/question", tags=["questions"])
 
 @app1.on_event("startup")
 def startup_event():
@@ -21,7 +22,7 @@ def shutdown_event():
 def read_root():
     return {"message": "Welcome to the FastAPI server!"}
 
-app1.include_router(question_router, prefix="/question", tags=["questions"])
+
 
 
 
