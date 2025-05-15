6f8ea1a0cad10b715d9cf51f2329c1d0e5e7a493
chaya <..>
2025-05-15 12:36:17 +0300

    create and finished

--- Server/app/db/Modules/posts.py
+++ Server/app/db/Modules/posts.py
@@ -1,37 +1,44 @@
 from pydantic import BaseModel
 
-class Quention(BaseModel):
+class Content(BaseModel):
+    context: str
+    userName: str
+
+class Post(BaseModel):
     Date: str
+    label: str
     Context: str
     Tags: list[int]
     UserId: str
+    Content: list[Content]
+    
 
-class QuentionModel:
-    def __init__(self, quention_model):
-        self.quention_model = quention_model
+class PostModel:
+    def __init__(self, post_model):
+        self.post_model = post_model
 
-    def get_all_quentions(self):
-        response = self.quention_model.getAllItems()  
+    def get_all_posts(self):
+        response = self.post_model.getAllItems()  
         if response:
             response = [{**item, "_id": str(item["_id"])} for item in response]
             return response
         else:
             return {"message : response is not found"}
 
-    def create_quention(self, quention_data):
-        result = self.quention_model.add_item(quention_data.dict())
+    def create_post(self, post_data):
+        result = self.post_model.add_item(post_data.dict())
         return {"inserted_id": str(result.inserted_id)} if result else {"message": "Insertion failed"}
 
-    def get_last_quention(self):
-        response = self.get_all_quentions()
+    def get_last_post(self):
+        response = self.get_all_posts()
         if response:
             response = sorted(response, key=lambda x: x['_id'], reverse=True)
-            quention = response[:1]
-            return quention[0] if quention else None
+            post = response[:1]
+            return post[0] if post else None
         else:
             return None
-
-    def get_quentions_by_tags(self, tags):
+    
+    def get_posts_by_tags(self, tags):
         filter = {'Tags': {'$in': tags}}
         pipeline = [
             {'$match': filter},
@@ -40,7 +47,7 @@ class QuentionModel:
             }},
             {'$sort': {'match_count': -1}}
         ]
-        response = list(self.quention_model.aggregate(pipeline))
+        response = list(self.post_model.aggregate(pipeline))
         if response:
             response = [{**item, "_id": str(item["_id"])} for item in response]
             return response
---
+++ Server/app/routers/posts.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends
+from db.Services.dependencies import get_mongo_operations, get_db
+from functools import lru_cache
+from db.Modules.posts import PostModel, Post
+
+router = APIRouter(tags=["posts"])
+
+
+@lru_cache()
+def get_posts_service(
+    mongo_operations=Depends(lambda db=Depends(get_db): get_mongo_operations("posts", db))
+):
+    """
+    Dependency to provide a posts service with caching.
+    """
+    return PostModel(mongo_operations)
+
+@router.post("/createPost")
+def create_post(Post_data: Post, posts_service=Depends(get_posts_service)):
+    return posts_service.create_post(Post_data)
+
+@router.get("/getAllposts")
+def get_all_posts(posts_service=Depends(get_posts_service)):
+    return posts_service.get_all_posts()
+
+@router.get("/getLastPost")
+def get_last_post(posts_service=Depends(get_posts_service)):
+    return posts_service.get_last_post()
+
+@router.get("/getPostsByTags")
+def get_posts_by_tags(tags: str, posts_service=Depends(get_posts_service)):
+    tags_list = [int(tag) for tag in tags.split(",")]
+    return posts_service.get_posts_by_tags(tags_list)
